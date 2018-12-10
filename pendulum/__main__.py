fa120a7c6f262050f009da5eddd3954ffa105522
Tom Lavenziano <tlavenziano@gmail.com>
2018-12-10 10:36:49 -0500

    Datetime extraction almost works

--- pendulum/__main__.py
+++ pendulum/__main__.py
@@ -1,4 +1,8 @@
+import sys
 from pendulum import main
 
 print('Starting Pendulum...')
-main()
+# cli_input = sys.argv[1]
+# main(cli_input)
+note = "Bring my car in for an oil change by saturday"
+main(note)
--- pendulum/pendulum.py
+++ pendulum/pendulum.py
@@ -1,5 +1,21 @@
-def main():
-    print('TESTSS')
+import nlp
+import sentiment
+import assemble
+
+
+def main(note):
+    return note_to_smartnote(note)
+
+
+def note_to_smartnote(note):
+    # (content, datetime) = nlp.process(note)
+    smart_note = nlp.process(note)
+
+
+    # smart_note = note + ' THAT IS NOW SUPER SMART'
+    print(smart_note)
+    return smart_note
+
 
 def testPendulumLoaded():
     return 'Pendulum Loaded'
