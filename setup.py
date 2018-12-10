b5dd62b4e539ed669bec2b611add33b842f149f5
Tom Lavenziano <tlavenziano@gmail.com>
2018-12-10 05:34:36 -0500

    NLTK data now downloads automatically during setup/install

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
+import nltk
+
+nltk.download('popular', download_dir='./env/nltk_data')
 
 setup(name='pendulum',
-      version='0.1',
+      version='1.0',
       description='Intelligent chronoparsing',
-      url='https://github.com/',
-      license='GPLv3',
+      url='https://github.com/TomLavenziano/Pendulum/',
+      license='LGPLv3',
       packages=['pendulum'],
       zip_safe=False)
