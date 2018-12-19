5569cf6dd81c3405a9b864ce15f83592fc9dbf89
Tom Lavenziano <tlavenziano@gmail.com>
2018-12-19 17:45:01 -0500

    Update README with better examples

--- pendulum/__main__.py
+++ pendulum/__main__.py
@@ -2,7 +2,6 @@ import sys
 from pendulum import main
 
 print('Starting Pendulum...')
-# cli_input = sys.argv[1]
-# main(cli_input)
-note = "Bring my car in for an oil change by saturday"
+
+note = "Bring my car in for an oil change by this saturday"
 main(note)
--- pendulum/nlp.py
+++ pendulum/nlp.py
@@ -4,19 +4,16 @@ import time_parser
 import assemble
 import sentiment
 
-now = datetime.datetime.now()
-
 def process(note):
     marked = time_parser.mark(note)
-    print(marked)
-    dTime = time_parser.extract_parse_datetime(marked)
+    date_time = time_parser.extract_parse_datetime(marked)
     context = time_parser.extract_context(marked)
     smart_note = assemble.assemble(context)
     emotion = sentiment.empathize(context)
 
     return {
             "context": context,
-            "datetime": dTime,
+            "datetime": date_time,
             "sentiment": {
                 "polarity": emotion.polarity,
                 "confidence": emotion.subjectivity
